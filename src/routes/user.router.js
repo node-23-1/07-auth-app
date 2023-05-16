3a3f1640ceb45ce905cbe88c2f2739a646c260ca
Andres Mendoza <andres.david.mm@hotmail.com>
2023-05-15 22:10:42 -0500

    first commit

---
+++ src/routes/user.router.js
@@ -0,0 +1,24 @@
+const { getAll, create, getOne, remove, update, login, getLoggedUser } = require('../controllers/user.controllers');
+const express = require('express');
+const verifyJWT = require('../utils/verifyJWT');
+
+const userRouter = express.Router();
+
+userRouter.route('/')
+    .get(verifyJWT, getAll, )
+    .post(create);
+
+userRouter.route('/me') // /users/me
+    .get(verifyJWT, getLoggedUser)
+
+userRouter.route('/login')
+    .post(login);
+
+userRouter.route('/:id')
+    .get(verifyJWT, getOne)
+    .delete(verifyJWT, remove)
+    .put(update);
+
+
+
+module.exports = userRouter;
